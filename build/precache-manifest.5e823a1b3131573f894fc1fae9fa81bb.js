25e5548d063c66d775cbeee1491cc31d35d347b2
Gagandeepsingh10798 <gs593513@gmail.com>
2020-08-06 09:08:42 +0530

    first commit

---
+++ build/precache-manifest.5e823a1b3131573f894fc1fae9fa81bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce3bd7f8b693888f6d2dd4669a35f23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0beb1d2adfff77c85e4",
+    "url": "/static/css/main.84e8176a.chunk.css"
+  },
+  {
+    "revision": "d2d22d4bb6d559472c9e",
+    "url": "/static/js/2.d348633c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.d348633c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0beb1d2adfff77c85e4",
+    "url": "/static/js/main.91e298bd.chunk.js"
+  },
+  {
+    "revision": "01f3a7b44639c0f3643a",
+    "url": "/static/js/runtime-main.bc760419.js"
+  }
+]);
